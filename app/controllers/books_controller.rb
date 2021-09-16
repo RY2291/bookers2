ed93dc7b2fb1c910a202312d86a58931657c9304
RY2291 <prgr2498@gmail.com>
2021-09-16 10:53:28 +0000

    Add レイアウトbook/index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,8 +7,13 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      redirect_to book_path(@book.id)
+      flash[:notice] = "You have created book successfully."
+    else
+      @books = Book.all
+      render "index"
+    end
   end
 
   def show
@@ -27,8 +32,12 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
+  if @book.update(book_params)
     redirect_to book_path(@book.id)
+    flash[:notice] = "You have updated book successfully."
+  else
+    render "edit"
+  end
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,24 +4,28 @@ class UsersController < ApplicationController
     @book = Book.new
     @users = User.find(params[:id])
   end
-  
+
   def index
     @users = User.all
   end
-  
+
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
-    user = User.find(params[:id])
-    user.update(user_params)
-    redirect_to user_path(user.id)
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(user.id)
+      flash[:notice] = "You have update user successfully."
+    else
+      render "edit"
+    end
   end
-  
+
   private
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
-  
+
 end
