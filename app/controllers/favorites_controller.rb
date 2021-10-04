5a7264d98616b442bc6dcdcadb934a0f97bbd31b
RY2291 <prgr2498@gmail.com>
2021-10-04 02:15:32 +0000

    start 非同期通信

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -7,14 +7,14 @@ class FavoritesController < ApplicationController
     favorite.user_id = current_user.id
     favorite.book_id = book.id
     favorite.save
-    redirect_back(fallback_location: root_path)
+    
   end
   
   def destroy
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    redirect_back(fallback_location: root_path)
+   
   end
 
 end
