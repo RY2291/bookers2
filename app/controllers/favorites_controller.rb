b2a8eb1100fd35fa8d687e716dcb592a7a546d40
RY2291 <prgr2498@gmail.com>
2021-10-04 06:00:04 +0000

    add いいね（非同期）

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,20 +1,17 @@
 class FavoritesController < ApplicationController
   
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     #favorite = current_user.favorites.new(book_id: book.id)
     favorite = Favorite.new
     favorite.user_id = current_user.id
-    favorite.book_id = book.id
+    favorite.book_id = @book.id
     favorite.save
-    
   end
   
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-   
   end
-
 end
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,5 @@
 class SearchesController < ApplicationController
-
+before_action :authenticate_user!
     def searches
         if params[:search].present?
             if params[:model] == "user"
