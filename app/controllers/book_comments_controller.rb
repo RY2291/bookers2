a71fd81fa0ff58cb84e502913b06a6bc84af6dde
RY2291 <prgr2498@gmail.com>
2021-10-05 06:16:10 +0000

    finish コメント非同期通信

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,17 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    book_comment = BookComment.new(comment_params)
-    book_comment.user_id = current_user.id
-    book_comment.book_id = book.id
-    book_comment.save
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.new(comment_params)
+    @book_comment.user_id = current_user.id
+    @book_comment.book_id = @book.id
+    @book_comment.save
+    @user = current_user
   end
 
   def destroy
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
+    @book = Book.find(params[:book_id])
+    @user = current_user
   end
 
   private
